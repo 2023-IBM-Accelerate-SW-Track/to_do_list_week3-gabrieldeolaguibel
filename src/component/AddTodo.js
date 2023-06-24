c2f40bf6116ff19da140876813bbaa0d0927e4dd
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2023-06-23 23:39:25 -0500

    Updated AddTodo.js

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,63 +1,70 @@
 import React, { Component } from "react";
-import { Button, TextField } from "@mui/material";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 class AddTodo extends Component {
-  // Create a local react state of the this component with both content date property set to nothing.
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       content: "",
-      date: ""
+      date: new Date(),
+      due: null
     };
   }
-  // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
-  // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
-  // into the text field.
+
   handleChange = (event) => {
     this.setState({
       content: event.target.value,
-      date: Date().toLocaleString('en-US')
+      date: new Date().toLocaleDateString()
     });
   };
-  // The handleSubmit function collects the forms input and puts it into the react state.
-  // event.preventDefault() is called to prevents default event behavior like refreshing the browser.
-  // this.props.addTodo(this.state) passes the current state (or user input and current date/time) into the addTodo function defined
-  // in the Home.js file which then adds the input into the list.
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if (this.state.content.trim()) {
-      this.props.addTodo(this.state);
-      this.setState({
-        content: "",
-        date: ""
-      });
-    }
+
+  handleDateChange = (event) => {
+    this.setState({
+      due: new Date(event).toLocaleDateString()
+    });
   };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.addTodo({
+      id: Date.now(),
+      content: this.state.content,
+      date: this.state.date,
+      due: this.state.due
+    });
+    this.setState({
+      content: "",
+      date: new Date(),
+      due: null
+    });
+  };
+
   render() {
     return (
-      // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
-      // one div element.
-      // 2. The return statement should include a text field input with the handleChange function from above that
-      // is passed into an onChange event.
-      // 3. The return should also include a button with the handleSubmit function from above that is passed into
-      // an OnClick event.
-      // 4. The value of the text field also should reflect the local state of this component.
-      <div>
+      <form onSubmit={this.onSubmit}>
         <TextField
+          id="new-item"
           label="Add New Item"
-          variant="outlined"
-          onChange={this.handleChange}
           value={this.state.content}
+          onChange={this.handleChange}
+          fullWidth
         />
-        <Button
-          style={{ marginLeft: "10px" }}
-          onClick={this.handleSubmit}
-          variant="contained"
-          color="primary"
-        >
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DesktopDatePicker
+            id="new-item-date"
+            label="Due Date"
+            value={this.state.due}
+            onChange={this.handleDateChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+        <Button variant="contained" color="primary" type="submit">
           Add
         </Button>
-      </div>
+      </form>
     );
   }
 }
